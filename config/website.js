0229b461e63ced94d0c24416e67d2b4eb63d5694
agustinus.nathaniel <agustinus.nathaniel@sirclo.co.id>
2019-07-11 02:59:58 +0700

    Mantap

--- config/website.js
+++ config/website.js
@@ -3,23 +3,23 @@ const tailwind = require('../tailwind')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteTitleShort: 'Cara', // short_name for manifest
-  siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
-  siteUrl: 'https://cara.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Agustinus Nathaniel', // Navigation and Site Title
+  siteTitleAlt: 'nate_228', // Alternative Site title for SEO
+  siteTitleShort: 'nathan', // short_name for manifest
+  siteHeadline: 'Agustinus Nathaniel\'s Personal Site', // Headline for schema.org JSONLD
+  siteUrl: 'https://agustinusnathaniel.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
   siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  author: 'Agustinus Nathaniel', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@cara', // Twitter Username
-  ogSiteName: 'cara', // Facebook Site Name
+  userTwitter: '@sozonome', // Twitter Username
+  ogSiteName: 'nate_228', // Facebook Site Name
   ogLanguage: 'en_US', // Facebook Language
-  googleAnalyticsID: 'UA-47519312-5',
+  googleAnalyticsID: '',
 
   // Manifest and Progress color
-  themeColor: tailwind.colors.orange,
-  backgroundColor: tailwind.colors.blue,
+  themeColor: tailwind.colors.teal,
+  backgroundColor: tailwind.colors.orange,
 }
