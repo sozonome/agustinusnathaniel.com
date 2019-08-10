bc1cd376018c62136eaebe7db47f491051e57ce9
agustinus.nathaniel <agustinus.nathaniel@sirclo.co.id>
2019-08-10 13:20:52 +0700

    Update Links

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Head from './Head';
 import { useStaticQuery, graphql } from 'gatsby';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
@@ -40,91 +39,37 @@ export default function Links() {
         <div className="container">
           <div className="title">Find Me Here</div>
           <div className="container">
-            <div className="container">
-              <h5 className="title is-5">Visuals</h5>
-              <div className="level is-mobile">
-                <div className="level-left">
-                  <div className="level-item">
-                    <a href={'https://www.instagram.com/' + site.siteMetadata.social.instagram}>
-                      <FontAwesomeIcon icon={['fab', 'instagram']} size="2x" />
-                    </a>
-                  </div>
-                  <div className="level-item">
-                    <a href={'https://www.youtube.com/' + site.siteMetadata.social.youtube}>
-                      <FontAwesomeIcon icon={['fab', 'youtube']} size="2x" />
-                    </a>
-                  </div>
-                  <div className="level-item">
-                    <a href={'http://be.net/' + site.siteMetadata.social.behance}>
-                      <FontAwesomeIcon icon={['fab', 'behance']} size="2x" />
-                    </a>
-                  </div>
-                  <div className="level-item">
-                    <a href={'https://www.dribbble.com/' + site.siteMetadata.social.dribbble}>
-                      <FontAwesomeIcon icon={['fab', 'dribbble']} size="2x" />
-                    </a>
-                  </div>
-                  <div className="level-item">
-                    <a href={'https://www.unsplash.com/' + site.siteMetadata.social.unsplash}>
-                      <FontAwesomeIcon icon={faCamera} size="2x" />
-                    </a>
-                  </div>
+            <div className="level is-mobile">
+              <div className="level-left">
+                <div className="level-item">
+                  <a href={'https://www.instagram.com/' + site.siteMetadata.social.instagram}>
+                    <FontAwesomeIcon icon={['fab', 'instagram']} size="2x" />
+                  </a>
                 </div>
-              </div>
-            </div>
-            <br />
-            <div className="container">
-              <h5 className="title is-5">Tech</h5>
-              <div className="level is-mobile">
-                <div className="level-left">
-                  <div className="level-item">
-                    <a href={'https://www.github.com/' + site.siteMetadata.social.github}>
-                      <FontAwesomeIcon icon={['fab', 'github']} size="2x" />
-                    </a>
-                  </div>
-                  <div className="level-item">
-                    <a href={'https://www.dev.to/' + site.siteMetadata.social.devto}>
-                      <FontAwesomeIcon icon={['fab', 'dev']} size="2x" />
-                    </a>
-                  </div>
+                <div className="level-item">
+                  <a href={'https://www.youtube.com/' + site.siteMetadata.social.youtube}>
+                    <FontAwesomeIcon icon={['fab', 'youtube']} size="2x" />
+                  </a>
                 </div>
-              </div>
-            </div>
-            <br />
-            <div className="container">
-              <h5 className="title is-5">Journals</h5>
-              <div className="level is-mobile">
-                <div className="level-left">
-                  <div className="level-item">
-                    <a href={'https://linkedin.com/in/' + site.siteMetadata.social.linkedin}>
-                      <FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" />
-                    </a>
-                  </div>
-                  <div className="level-item">
-                    <a href={'https://www.medium.com/' + site.siteMetadata.social.medium}>
-                      <FontAwesomeIcon icon={['fab', 'medium']} size="2x" />
-                    </a>
-                  </div>
-                  <div className="level-item">
-                    <a href={site.siteMetadata.social.apple_podcast}>
-                      <FontAwesomeIcon icon={faPodcast} size="2x" />
-                    </a>
-                  </div>
-                  <div className="level-item">
-                    <a href={site.siteMetadata.social.spotify_podcast}>
-                      <FontAwesomeIcon icon={['fab', 'spotify']} size="2x" />
-                    </a>
-                  </div>
-                  <div className="level-item">
-                    <a href={site.siteMetadata.social.spotify_playlist}>
-                      <FontAwesomeIcon icon={faHeadphones} size="2x" />
-                    </a>
-                  </div>
-                  <div className="level-item">
-                    <a href={'https://twitter.com/' + site.siteMetadata.social.twitter}>
-                      <FontAwesomeIcon icon={['fab', 'twitter']} size="2x" />
-                    </a>
-                  </div>
+                <div className="level-item">
+                  <a href={'https://www.unsplash.com/' + site.siteMetadata.social.unsplash}>
+                    <FontAwesomeIcon icon={faCamera} size="2x" />
+                  </a>
+                </div>
+                <div className="level-item">
+                  <a href={'https://www.github.com/' + site.siteMetadata.social.github}>
+                    <FontAwesomeIcon icon={['fab', 'github']} size="2x" />
+                  </a>
+                </div>
+                <div className="level-item">
+                  <a href={site.siteMetadata.social.spotify_podcast}>
+                    <FontAwesomeIcon icon={['fab', 'spotify']} size="2x" />
+                  </a>
+                </div>
+                <div className="level-item">
+                  <a href={'https://twitter.com/' + site.siteMetadata.social.twitter}>
+                    <FontAwesomeIcon icon={['fab', 'twitter']} size="2x" />
+                  </a>
                 </div>
               </div>
             </div>
