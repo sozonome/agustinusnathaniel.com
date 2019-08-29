658c04c06ee19ce42a976fe280101c0d446e4625
agustinus.nathaniel <agustinus.nathaniel@sirclo.co.id>
2019-08-29 23:00:03 +0700

    Add serviceWorker (PWA WIP)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,11 @@
 import "./src/styles/global.scss"
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+  if (answer === true) {
+    window.location.reload()
+  }
+} 
--- gatsby-config.js
+++ gatsby-config.js
@@ -54,6 +54,7 @@ module.exports = {
         // Any invalid keyword or empty string defaults to `anonymous`
         crossOrigin: `use-credentials`,
       },
-    }
+    },
+    `gatsby-plugin-offline`,
   ],
 }
