890e49c7117f85948b8242b9ee41590f3557a02a
sozonome <berserk.nate@gmail.com>
2020-04-07 22:22:45 +0700

    Title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,11 @@ import Layout from '../components/Layout';
 export default function IndexPage() {
   return (
     <Layout>
-      <div className="sm:flex px-6 items-end justify-center h-screen">
-        <h1 className="flex w-full sm:w-1/2 text-5xl sm:text-6xl font-bold sm:justify-end">Hi! 👋</h1>
-        <h2 className="w-full sm:w-1/2 text-4xl sm:text-5xl font-semibold">I'm <span className="font-bold">Nathan</span></h2>
+      <div className="px-6 justify-center h-screen">
+        <div className="sm:flex items-end">
+          <h1 className="flex w-full sm:w-1/2 text-5xl sm:text-6xl font-bold sm:justify-end">Hi! 👋</h1>
+          <h2 className="w-full sm:w-1/2 text-4xl sm:text-5xl font-semibold">I'm <span className="font-bold">Nathan</span></h2>
+        </div>
       </div>
     </Layout>
   )
