a0f5e33eb3d6a6c36acf02e25f2ee52662aca835
sozonome <berserk.nate@gmail.com>
2020-04-08 14:19:00 +0700

    Site Stack + Daily Use

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -27,29 +27,50 @@ export default function About() {
     <Layout>
       <div>
         <h1 className="text-3xl font-bold">About me</h1>
-        <p className="text-xs text-gray-600">Last update : 08 April 2020. <br/> (in progress doing thesis for graduation)</p>
+        <p className="text-xs text-gray-600">Last update : 08 April 2020. <br/> (in progress doing thesis ✌️)</p>
         <section id="intro" className="mt-4">
           <p className="mb-4 text-md">
-            I'm Agustinus Nathaniel, currently an IT (Computer Science / Informatics) Student from Indonesia.
+            I'm Agustinus Nathaniel, currently an IT (Computer Science / Informatics) Student from Indonesia. 
             {/* I aim to be a Front End Developer after graduate in several more months. */}
+            <br/>
             I love to explore and learn stuffs around technology and design.
           </p>
-          <h2 className="text-2xl font-semibold mb-2">How do I got into tech?</h2>
+          <h2 className="text-2xl font-semibold mb-2">How do I got into Tech?</h2>
           <div className="mb-4 text-sm">
-            I was fascinated by computer technology since I was so small. Then, internet is becoming more accessible. 
+            I was fascinated by computer technology since I was little. Then, internet is becoming more accessible. 
             I started to know Google, Yahoo!, blogger, facebook, twitter, etc. I became interested in web technologies and applications.
             But I took it for granted, I just thought my interest towards computer and web technologies is just the same as my other friends, so I didn't feel special at all having that interest.
             I started to have so many interests towards various things.
-            Even when I was looking for college at my final year of high school, IT is not my first option. Long story short, after applying myself to some college with non-IT major and scholarship programs, I finally took IT major.
-            The first 2 year of college is full of stuggling because I just realized there are so many courses and I only like almost 20% of them. 
-            The only subjects I really like is only about web and mobile development.
+            Even when I was looking for college at my final year of high school, IT is not my first option. Long story short, after applying myself to some college with non-IT major and scholarship programs (which I also failed to get), I finally took IT major.
+            The first 2 year of college is full of stuggling because there are so many courses and I like almost 20% of them only. 
+            The only subjects I really like is subjects around web and mobile development.
+            Hoping to graduate soon in several more months.
           </div>
-          {/* <h2 className="text-2xl font-semibold mb-2">Beside being an IT Student...</h2>
+          <h2 className="text-2xl font-semibold mb-2">Beside Tech...</h2>
           <div className="mb-4 text-sm">
-            I also do freelance website development, photography and videography services.
-            I share my personal shoots (mainly travel content) on my <a href={'https://www.instagram.com/' + site.siteMetadata.social.instagram} className="w-8 text-orange-600 hover:text-blue-400 underline hover:font-bold">Instagram</a>.
+            I also do photography and videography.
+            <br/>
+            {/* <div className="mb-2">
+              (Check out my works <a href="https://www.instagram.com/cherocaptures" className="text-blue-300 font-semibold underline hover:text-blue-200" target="_blank">here</a>.)
+            </div> */}
+            I share my personal shoots (mainly travel content) on my <a href={'https://www.instagram.com/' + site.siteMetadata.social.instagram} target="_blank" className="w-8 text-orange-600 hover:text-blue-400 underline hover:font-bold">Instagram</a>.
             <div className="mt-2 text-xs">N.B. Yeah I'm so used to people assumed myself as a film / photography student 😂.</div>
-          </div> */}
+          </div>
+          <h2 className="text-2xl font-bold mt-12 text-teal-400">Lists</h2>
+          <h4 className="text-1xl font-semibold mb-2">Some lists or recommendations of stuffs I like and/or use.</h4>
+          <div className="mb-4 text-md">
+            <ul>
+              <li className="mb-2">
+                <Link to="/dailyuse" className="text-teal-500 hover:text-teal-200 font-semibold hover:underline">What I use daily</Link>
+              </li>
+              <li className="mb-2">
+                <Link to="/sitestack" className="text-teal-500 hover:text-teal-200 font-semibold hover:underline">This site stacks</Link>
+              </li>
+              <li>
+                <a href="https://github.com/sozonome/great-apps-sites" className="text-teal-500 hover:text-teal-200 font-semibold hover:underline" target="_blank">Fun Apps / Sites</a>
+              </li>
+            </ul>
+          </div>
         </section>
         <section id="content-foot">
         </section>
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -4,7 +4,10 @@ import Layout from '../components/Layout'
 export default function blog() {
   return (
     <Layout>
-      Coming Soon
+      <div className="text-gray-400">
+        <h1 className="text-4xl font-bold">Coming Soon</h1>
+        <p className="text-2xl">still in working progress</p>
+      </div>
     </Layout>
   )
 }
