6fd714e7987416eaea195890f8d64066960f7635
sozonome <berserk.nate@gmail.com>
2020-04-09 15:44:17 +0700

    share button

--- src/templates/blogTemplate.tsx
+++ src/templates/blogTemplate.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import {graphql} from "gatsby";
 import Layout from '../components/Layout';
-import configQuery from '../../data/siteConfig';
+import Helmet from 'react-helmet';
+import { library, config } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+const configQuery = require('../../data/siteConfig');
+
+config.autoAddCss = false;
+library.add(fab);
 
 export default function blogTemplate({
   data
@@ -10,23 +17,38 @@ export default function blogTemplate({
   const { frontmatter, html } = markdownRemark
   const { siteMetadata  } = configQuery
   const shareTwtr = `http://twitter.com/share?text=${frontmatter.title}&url=${siteMetadata.siteUrl}${frontmatter.path}/&via=sozonome`
+  // const shareFb = `https://www.facebook.com/plugins/share_button.php?href=${siteMetadata.siteUrl}${frontmatter.path}&t=${frontmatter.title}&layout=button&size=small&width=79&height=20&appId`
+  // const shareFbLg = `https://www.facebook.com/plugins/share_button.php?href=${siteMetadata.siteUrl}${frontmatter.path}&t=${frontmatter.title}&layout=button&size=small&width=91&height=28&appId`
 
   return (
     <Layout>
+      <Helmet>
+        <title>{frontmatter.title} | {siteMetadata.title}</title>
+      </Helmet>
       <div className="post">
         <div className="postContainer">
           <div id="post-head" className="mb-8">
             <h1 className="font-bold text-4xl mb-2">{frontmatter.title}</h1>
             <div className="flex items-center h-10">
               <h2 className="text-sm mr-4">{frontmatter.date}</h2>
-              <a href={shareTwtr} className="text-blue-600 font-bold text-sm hover:font-extrabold hover:text-blue-400" target="_blank" rel="noopener">Share</a>
+              <a href={shareTwtr} className="flex text-blue-600 font-bold text-sm hover:font-extrabold hover:text-blue-400" target="_blank" rel="noopener">
+                <FontAwesomeIcon className="w-4 mr-1" icon={['fab', 'twitter']} size="xs"/> 
+                <div>Share</div>
+              </a>
+              {/* <iframe className="ml-2" src={shareFb} width="100" height="32" style={{border:"none",overflow:"hidden"}} scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe> */}
             </div>
           </div>
           <div
             className="mkdown mb-6"
             dangerouslySetInnerHTML={{ __html: html }}
           />
-          <a href={shareTwtr} className="py-2 px-4 font-bold text-sm bg-blue-600 hover:bg-blue-400 rounded-md" target="_blank" rel="noopener">Share</a>
+          <div className="flex items-center">
+            <a href={shareTwtr} className="flex py-2 px-4 font-bold text-sm bg-blue-600 hover:bg-blue-400 rounded-md" target="_blank" rel="noopener">
+              <FontAwesomeIcon className="w-4 mr-1" icon={['fab', 'twitter']} size="xs"/> 
+              <div>Share</div>
+            </a>
+            {/* <iframe className="ml-2" src={shareFbLg} width="100" height="56" style={{border:"none",overflow:"hidden"}} scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe> */}
+          </div>
         </div>
       </div>
     </Layout>
