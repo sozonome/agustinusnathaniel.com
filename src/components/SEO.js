3a546c1269ac125f5f6549b83ddb47a71d002f2e
sozonome <berserk.nate@gmail.com>
2020-04-22 14:26:14 +0700

    Preparing SEO Helmet, better meta for Link Preview / Card

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { useStaticQuery, graphql } from 'gatsby';
+import React from "react"
+import Helmet from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
 
 export default function SEO() {
   const { site } = useStaticQuery(graphql`
@@ -12,18 +12,32 @@ export default function SEO() {
         }
       }
     }
-  `);
+  `)
 
   return (
     <Helmet>
-      <html lang="en"/>
+      <html lang="en" />
+      {/* <!-- Primary Meta Tags --> */}
       <title>{site.siteMetadata.title}</title>
-      <meta name="og:type" content="website" />
-      <meta name="og:title" content={site.siteMetadata.title} />
-      <meta name="og:description" content={site.siteMetadata.description} />
-      <meta name="Description" content={site.siteMetadata.description} />
+      {/* <meta name="title" content={""} /> */}
+      <meta name="description" content={site.siteMetadata.description} />
+
+      {/* <!-- Open Graph / Facebook --> */}
+      <meta property="og:type" content="website" />
+      {/* <meta property="og:url" content={""} /> */}
+      <meta property="og:title" content={site.siteMetadata.title} />
+      <meta property="og:description" content={site.siteMetadata.description} />
+      {/* <meta property="og:image" content={""} /> */}
+
+      {/* <!-- Twitter --> */}
+      {/* <meta property="twitter:card" content="summary_large_image" /> */}
+      {/* <meta property="twitter:url" content={""} /> */}
+      {/* <meta property="twitter:title" content={""} /> */}
+      {/* <meta property="twitter:description" content={""} /> */}
+      {/* <meta property="twitter:image" content={""} /> */}
+
+      
       <link rel="shortcut icon" href="favicon.ico" />
     </Helmet>
   )
-
 }
