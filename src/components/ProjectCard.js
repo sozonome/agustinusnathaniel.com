eb9df91bd37d33fddac023c6edde0a57bfb84637
sozonome <berserk.nate@gmail.com>
2020-05-16 00:04:43 +0700

    Undo Responsive Project Page, revert back to Single Item per Row

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,36 +1,37 @@
 import React from "react"
 
-const ProjectCard = (props) => {
+const ProjectCard = props => {
   return (
-    <div className="projectCard w-full sm:w-1/2 sm:flex-none my-2" key={props.key}>
-      <div className="bg-gray-900 hover:bg-blue-800 p-4 rounded-lg">
-        <h2 className="text-2xl font-bold">{props.value.title}</h2>
-        <p className="text-sm mb-4">{props.value.description}</p>
-        <div className="flex">
-          {props.value.projectLink ? (
-            <a
-              className="font-bold bg-teal-800 hover:bg-orange-600 hover:text-white rounded-lg p-2 mr-2"
-              href={props.value.projectLink}
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              Visit
-            </a>
-          ) : null}
-          {props.value.repoLink ? (
-            <a
-              className="font-bold border border-blue-200 hover:bg-white hover:text-black rounded-lg p-2 mr-2"
-              href={props.value.repoLink}
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              Source
-            </a>
-          ) : null}
-        </div>
+    <div
+      key={props.key}
+      className="w-full my-2 bg-gray-900 hover:bg-blue-800 p-4 rounded-lg"
+    >
+      <h2 className="text-2xl font-bold">{props.value.title}</h2>
+      <p className="text-sm mb-4">{props.value.description}</p>
+      <div className="flex">
+        {props.value.projectLink ? (
+          <a
+            className="font-bold bg-teal-800 hover:bg-orange-600 hover:text-white rounded-lg p-2 mr-2"
+            href={props.value.projectLink}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Visit
+          </a>
+        ) : null}
+        {props.value.repoLink ? (
+          <a
+            className="font-bold border border-blue-200 hover:bg-white hover:text-black rounded-lg p-2 mr-2"
+            href={props.value.repoLink}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Source
+          </a>
+        ) : null}
       </div>
     </div>
   )
 }
 
-export default ProjectCard;
+export default ProjectCard
