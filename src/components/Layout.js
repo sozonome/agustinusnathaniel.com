f431ced0e6eb9f7ec765bdf94ba5d3096cb7645b
sozonome <berserk.nate@gmail.com>
2020-05-20 15:28:16 +0700

    Remove styling from helmet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,26 +1,20 @@
-import React, { Component } from 'react'
-import SEO from './SEO';
-import 'typeface-muli';
-import 'typeface-open-sans';
-import Helmet from 'react-helmet';
-import Head from './Head';
-import Foot from './Foot';
+import React, { Component } from "react"
+import SEO from "./SEO"
+import "typeface-muli"
+import "typeface-open-sans"
+import Head from "./Head"
+import Foot from "./Foot"
 
 export default class Layout extends Component {
   render() {
     return (
-      <div className="flex flex-wrap mainwrapper mx-auto h-screen">
+      <div className="flex flex-wrap mainwrapper bg-black text-white mx-auto">
         <SEO />
-        <Helmet>
-          <body className="bg-black text-white"/>
-        </Helmet>
-        <Head/>
+        <Head />
         <div id="site-content" className="my-8 px-6 w-full">
-          <div className="fadeTrans">
-            {this.props.children}
-          </div>
+          <div className="fadeTrans">{this.props.children}</div>
         </div>
-        <Foot/>
+        <Foot />
       </div>
     )
   }
