70dcb9d32a1e6322089b4498e954df8c6d75cb5a
sozonome <berserk.nate@gmail.com>
2020-05-20 15:01:29 +0700

    Test Changing PurgeCSS Settings

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,1 @@
-const {renderToString} = require("react-dom/server")
-
-exports.replaceRenderer = ({bodyComponent, replaceBodyHTMLString}) => {
-  const bodyHTML = renderToString(bodyComponent)
-
-  replaceBodyHTMLString(bodyHTML);
-}
+// Gatsby SSR
