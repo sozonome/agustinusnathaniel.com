9073c3a18ce2355a32ae417bfc1ed6ab5af881e2
sozonome <berserk.nate@gmail.com>
2020-06-04 12:12:59 +0700

    📖Add some personal handle

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -1,23 +1,52 @@
 const siteConfig = {
-  siteMetadata:{
+  siteMetadata: {
     title: `Agustinus Nathaniel`,
     description: `Tech and Design enthusiast.`,
     author: `sozonome`,
     siteUrl: `https://agustinusnathaniel.com`, // Domain of your site. No trailing slash!
-    social:{
+    social: {
+      // General
       youtube: `AgustinusNathaniel`,
+      instagram: `agustinusnathaniel`,
+      twitter: `sozonome`,
       linkedin: `agustinusnathaniel`,
+      pinterest: `agustinusnathaniel`,
+      email: `hello@agustinusnathaniel.com`,
+      medium: `@agustinusnathaniel`,
+
+      // Design, Art
+      behance: `agustinusnathaniel`,
+      dribbble: `agustinusnathaniel`,
+
+      // Dev
       github: `sozonome`,
       devto: `sozonome`,
-      twitter: `sozonome`,
-      instagram: `agustinusnathaniel`,
-      medium: `@agustinusnathaniel`,
-      behance: `agustinusnathaniel`,
-      email: `hello@agustinusnathaniel.com`,
+      indiehackers: `sozonome`,
+      devInstagram: `sozonome`,
+      codersrank: `sozonome`,
+      npmjs: `sozonome`,
+      stackshare: `sozonome`,
+      devrant: `sozonome`,
+      codesandbox: `sozonome`,
+      freeCodeCamp: `sozonome`,
+      devSite: `https://sznm.dev`,
+      producthunt: `@sozonome`,
+
+      // Music
       spotify: `214cjghp4c3lzsygqz73qvuxi`,
-      spotify_podcast: `https://open.spotify.com/show/0ORzchlOopGccuNlQ5SAyy`,
-      apple_podcast: `https://podcasts.apple.com/id/podcast/decompiling-fruits/id1452637576`,
-      spotify_playlist: `https://open.spotify.com/user/214cjghp4c3lzsygqz73qvuxi`,
+
+      // Podcast - Decompiling Fruits
+      decompiling_fruits: {
+        anchor_fm: `https://anchor.fm/decompilingfruits`,
+        spotify_podcast: `https://open.spotify.com/show/0ORzchlOopGccuNlQ5SAyy`,
+        apple_podcast: `https://podcasts.apple.com/id/podcast/decompiling-fruits/id1452637576`,
+      },
+
+      // Podcast - Podkes Hiya Hiya
+      hiya_hiya: {
+        anchor_fm_hiya: `https://anchor.fm/hiyahiya`,
+        spotify_podcast: `https://open.spotify.com/show/0bKujtejI8OxI50df9yxf9`,
+      },
     },
   },
 }
