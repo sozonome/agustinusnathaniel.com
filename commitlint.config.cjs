2ab2a4faccb2d77d9fa7106539fdfc1a2528a4f9
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-05-28 13:07:17 +0700

    chore: update packages to latest

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,12 +1,14 @@
 /** @type {import('@commitlint/types').UserConfig} */
 const CommitLintConfiguration = {
 	extends: ['@commitlint/config-conventional'],
-	'scope-enum': [
-		2,
-		'always',
-		['analytics', 'blog', 'components', 'layout', 'pages', 'utils', 'constants']
-	],
-	'scope-case': [2, 'always', 'kebab-case']
+	rules: {
+		'scope-enum': [
+			2,
+			'always',
+			['analytics', 'blog', 'components', 'layout', 'pages', 'utils', 'constants']
+		],
+		'scope-case': [2, 'always', 'kebab-case']
+	}
 };
 
 module.exports = CommitLintConfiguration;
