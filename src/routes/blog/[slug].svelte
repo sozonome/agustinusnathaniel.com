97550c5e314a838fcd3e98ddfa296bb4b15e396a
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-08-28 22:57:38 +0700

    chore: move loader

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -1,5 +1,40 @@
 <script context="module" lang="ts">
-	export { load } from '$lib/routes/blog/detail/loader';
+	import type { Load } from '@sveltejs/kit';
+
+	import { BASE_URL } from '$lib/constants/baseUrl';
+	import { buildOgImgUrl } from '$lib/utils/buildOgImgUrl';
+
+	import type { BlogPostDetailProps } from '$lib/routes/blog/detail/types';
+
+	export const load: Load<{ slug: string }, BlogPostDetailProps> = async ({ params, fetch }) => {
+		const blogPost = (await fetch(`/blog/data/${params.slug}`).then(async (res) =>
+			res.json()
+		)) as BlogPostDetailProps;
+
+		return {
+			props: {
+				...blogPost
+			},
+			stuff: {
+				seo: {
+					title: `${blogPost.meta.title} | Agustinus Nathaniel`,
+					openGraph: {
+						images: [
+							{
+								url: buildOgImgUrl({
+									heading: blogPost.meta.title,
+									text: `${BASE_URL}/blog/${params.slug}`
+								})
+							}
+						]
+					}
+				}
+			},
+			cache: {
+				maxage: 60
+			}
+		};
+	};
 </script>
 
 <script lang="ts">
--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,10 +1,39 @@
 <script context="module" lang="ts">
-	export { load } from '$lib/routes/blog/list/loader';
+	import type { Load } from '@sveltejs/kit';
+
+	import type { BlogPostEntries } from '$lib/services/notion/blog/entries/types';
+
+	import type { BlogPostListProps } from '$lib/routes/blog/list/types';
+	import { buildOgImgUrl } from '$lib/utils/buildOgImgUrl';
+	import { BASE_URL } from '$lib/constants/baseUrl';
+
+	export const load: Load<Record<string, string>, BlogPostListProps> = async ({ fetch }) => {
+		const blogPosts = (await fetch('/posts').then(
+			async (res) => await res.json()
+		)) as BlogPostEntries;
+
+		return {
+			props: {
+				blogPosts
+			},
+			stuff: {
+				seo: {
+					title: 'Blog Post',
+					openGraph: {
+						images: [{ url: buildOgImgUrl({ heading: 'Blog Post', text: `${BASE_URL}/blog` }) }]
+					}
+				}
+			},
+			cache: {
+				maxage: 60
+			}
+		};
+	};
 </script>
 
 <script lang="ts">
 	import BlogPostList from '$lib/components/blog/list.svelte';
-	import type { BlogPostListProps } from '$lib/routes/blog/list/types';
+	// import type { BlogPostListProps } from '$lib/routes/blog/list/types';
 
 	export let blogPosts: BlogPostListProps['blogPosts'];
 </script>
