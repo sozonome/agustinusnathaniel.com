885efedabb165c187165d09bc77e4ff6216a5358
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-08-29 08:47:14 +0700

    fix: revise class typo on header component

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -1,4 +1,4 @@
-<header class="z-10 stick top-0 backdrop-blur-6px bg-zinc-900/80">
+<header class="z-10 sticky top-0 backdrop-blur-6px bg-zinc-900/80">
 	<div class="layout py-2">
 		<a href="/" class="no-underline">
 			<h1 class="text-sm sm:text-base text-gray-400">A. Nathaniel</h1>
